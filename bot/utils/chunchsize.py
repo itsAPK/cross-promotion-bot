0bf0871e9b482be038963b4afea573943629e767
Apoorva Kumar <apkvandagadde2@gmail.com>
2021-06-05 11:14:46 +0530

    chuncksize fixed

--- bot/utils/chunchsize.py
+++
@@ -1,7 +0,0 @@
-from bot.database.models.channel_db import channel
-
-
-def chunck():
-    l=[r.channel_id for r in session.query(Channel).distinct()]
-    for i in range(0,len(l),):
-        yield l[i:i+Config.CHUNK_SIZE]
